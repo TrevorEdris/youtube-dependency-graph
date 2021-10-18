b3cef2deb9d48eadaa590ea1d8a44b0147dc7954
Trevor Edris <trevor.edris@gmail.com>
2021-10-17 20:10:41 -0400

    A bit more code skeleton

--- cmd/ydg/main.go
+++ cmd/ydg/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/TrevorEdris/youtube-dependency-graph/pkg/scrape"
 )
 
 func main() {
     fmt.Println("Hello, world!")
+
+    // TODO: Parse CLI args here using urfave/cli/v2
+    s := scrape.New()
+    s.Scrape("https://www.youtube.com/watch?v=iDIcydiQOhc")
 }
