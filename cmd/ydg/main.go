bb8b7a10bb35b9eba6c20b9000b076ae4d7e22f2
Trevor Edris <trevor.edris@gmail.com>
2021-10-19 10:21:34 -0400

    Parse CLI args with urfave/cli/v2

--- cmd/ydg/main.go
+++ cmd/ydg/main.go
@@ -1,43 +1,174 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/inconshreveable/log15"
+	"github.com/urfave/cli/v2"
 
 	"github.com/TrevorEdris/youtube-dependency-graph/app"
 )
 
-var (
-	log = log15.New("module", "ydg")
+const (
+	appName        = "ydg"
+	defaultVersion = "v0.0.0"
+
+	flagURL   = "url"
+	flagTitle = "title"
+	flagID    = "id"
 )
 
-func initLogging(logLevel string) {
-	log.SetHandler(log15.MultiHandler(
-		log15.StreamHandler(os.Stderr, log15.LogfmtFormat()),
+var (
+	cfg app.Config
+
+	appVersion = getVersion()
+	log        = log15.New("module", appName)
+)
+
+func getVersion() string {
+	v := os.Getenv("VERSION")
+	if v == "" {
+		log.Warn("No value for VERSION found, using default", "defaultVersion", defaultVersion)
+		return defaultVersion
+	}
+	return v
+}
+
+func initLogging(logLevel, logFmt string) {
+	lvl, err := log15.LvlFromString(logLevel)
+	if err != nil {
+		log.Warn("Invalid LOG_LEVEL specified, defaulting to info", "error", err)
+		lvl = log15.LvlInfo
+	}
+
+	var loggerFormat log15.Format
+	switch logFmt {
+	case "logfmt":
+		loggerFormat = log15.LogfmtFormat()
+	case "jsonfmt":
+		loggerFormat = log15.JsonFormat()
+	case "terminalfmt":
+		loggerFormat = log15.TerminalFormat()
+	default:
+		log.Warn("Invalid LOG_FMT specified, defaulting to terminalfmt", "LOG_FMT", logFmt)
+		loggerFormat = log15.TerminalFormat()
+	}
+
+	log.SetHandler(
 		log15.LvlFilterHandler(
-			log15.LvlInfo,
-			log15.Must.FileHandler("errors.json", log15.JsonFormat()),
+			lvl,
+			log15.StreamHandler(os.Stdout, loggerFormat),
 		),
-	))
+	)
+}
+
+func initSettings(c *cli.Context) error {
+	var err error
+	cfg, err = app.ParseConfig()
+	if err != nil {
+		log.Error("Unable to parse config", "error", err)
+		return err
+	}
+
+	initLogging(cfg.Log.LogLevel, cfg.Log.LogFmt)
+	return nil
+}
+
+func cliCreateGraphFromURL(c *cli.Context) error {
+	ydg, err := app.New(cfg, log)
+	if err != nil {
+		log.Error("Unable to create ydg app", "error", err)
+		return err
+	}
+
+	err = ydg.GraphFromURL(c.String(flagURL))
+	if err != nil {
+		log.Error("ydg execution failed", "error", err)
+		return err
+	}
+	return nil
+}
+
+func cliCreateGraphFromTitle(c *cli.Context) error {
+	ydg, err := app.New(cfg, log)
+	if err != nil {
+		log.Error("Unable to create ydg app", "error", err)
+		return err
+	}
+
+	err = ydg.GraphFromTitle(c.String(flagTitle))
+	if err != nil {
+		log.Error("ydg execution failed", "error", err)
+		return err
+	}
+	return nil
+}
+
+func cliCreateGraphFromID(c *cli.Context) error {
+	ydg, err := app.New(cfg, log)
+	if err != nil {
+		log.Error("Unable to create ydg app", "error", err)
+		return err
+	}
+
+	err = ydg.GraphFromID(c.String(flagID))
+	if err != nil {
+		log.Error("ydg execution failed", "error", err)
+		return err
+	}
+	return nil
+}
+
+func newApplication() *cli.App {
+	application := &cli.App{}
+	application.Name = appName
+	application.Version = appVersion
+	application.Before = initSettings
+	application.Commands = []*cli.Command{
+		{
+			Name:   "from-url",
+			Usage:  "Create a dependency graph from a URL",
+			Action: cliCreateGraphFromURL,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     flagURL,
+					Usage:    "The URL of the youtube video to begin the graph with",
+					Value:    "",
+					Required: true,
+				},
+			},
+		},
+		{
+			Name:   "from-title",
+			Usage:  "Create a dependency graph from a video title",
+			Action: cliCreateGraphFromTitle,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     flagTitle,
+					Usage:    "The title of the youtube video to begin the graph with",
+					Value:    "",
+					Required: true,
+				},
+			},
+		},
+		{
+			Name:   "from-id",
+			Usage:  "Create a dependency graph from a video title",
+			Action: cliCreateGraphFromID,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     flagID,
+					Usage:    "The id of the youtube video to begin the graph with",
+					Value:    "",
+					Required: true,
+				},
+			},
+		},
+	}
+	return application
 }
 
 func main() {
-	cfg, err := app.ParseConfig()
-	if err != nil {
-		fmt.Printf("ERROR: Unable to parse config: %s\n", err)
-		os.Exit(1)
-	}
-
-	ydg, err := app.New(cfg, log)
-	if err != nil {
-		fmt.Printf("ERROR: Unable to create app: %s\n", err)
-		os.Exit(1)
-	}
-
-	err = ydg.Run()
-	if err != nil {
-		fmt.Printf("ERROR: Application failed: %s\n", err)
-	}
+	ydg := newApplication()
+	ydg.Run(os.Args)
 }
--- go.mod
+++ go.mod
@@ -8,11 +8,13 @@ require (
 	github.com/joho/godotenv v1.4.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/stretchr/testify v1.7.0
+	github.com/urfave/cli/v2 v2.3.0
 	google.golang.org/api v0.58.0
 )
 
 require (
 	cloud.google.com/go v0.94.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
@@ -21,6 +23,8 @@ require (
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
