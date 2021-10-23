b72f569febc4575476fb190b22634cf0ca13bff6
Trevor Edris <trevor.edris@gmail.com>
2021-10-23 11:10:20 -0400

    Add simple navbar

---
+++ website/src/components/navbar.js
@@ -0,0 +1,28 @@
+import logo from '../logo.svg';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+function YDGNavbar() {
+	return (
+		<Navbar bg="dark" variant="dark" expand="xxl">
+			<Container>
+				<Navbar.Brand href="#home">
+					{' '}
+					<img alt="" src={logo} width="30" height="30" className="d-inline-block align-top" />YDG
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<NavDropdown title="New" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#from_title">From Title</NavDropdown.Item>
+							<NavDropdown.Item href="#from_url">From URL</NavDropdown.Item>
+							<NavDropdown.Item href="#from_id">From ID</NavDropdown.Item>
+						</NavDropdown>
+						<Nav.Link href="#import">Import</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default YDGNavbar;
